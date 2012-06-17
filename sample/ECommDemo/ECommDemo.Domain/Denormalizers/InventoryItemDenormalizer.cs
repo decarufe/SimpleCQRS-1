0198de5afddb047ef853cbd66ce82543b971e54f
Andrea Balducci <mtb.snowboard@gmail.com>
2012-06-17 23:18:10 +0200

    Added inventory items denormalizer
    Inventory items collection is populated with a denormalizer listening to
    InventoryItemCreatedEvent

---
+++ sample/ECommDemo/ECommDemo.Domain/Denormalizers/InventoryItemDenormalizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ECommDemo.Domain.InventoryContext;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using SimpleCqrs.Eventing;
+
+namespace ECommDemo.Domain.Denormalizers
+{
+    public class InventoryItemDenormalizer : IHandleDomainEvents<InventoryItemCreatedEvent>
+    {
+        public void Handle(InventoryItemCreatedEvent e)
+        {
+            var server = MongoServer.Create();
+            var db = server.GetDatabase("ecomm");
+            var collection = db.GetCollection("inventory");
+
+            var    item = new BsonDocument();
+            item.Add("itemid", e.ItemId);
+            item.Add("description", e.Description);
+            item.Add("_id", e.AggregateRootId);
+
+            collection.Save(item);
+        }
+    }
+}
